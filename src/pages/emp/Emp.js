caa120b52633664cb5bf83d9a1adc8305d1a12cd
rionqkd520@gmail.com <rionqkd520@gmail.com>
2023-05-11 00:13:37 +0900

    💄 Design: CSS 등 사용자 UI 디자인 변경

--- src/pages/emp/Emp.js
+++ src/pages/emp/Emp.js
@@ -1,6 +1,11 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Header from "../../component/common/Header";
+
 function Emp() {
     return (
         <>
+            <Header/>
             <div>Emp</div>
         </>
     );
