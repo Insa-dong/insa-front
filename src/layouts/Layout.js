92279a61ff6696d91e42dab3c2afaf2920e0fd83
imyueol <imyueol000@gmail.com>
2023-05-10 17:51:43 +0900

    commit

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,12 +1,16 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from "../component/common/Navbar";
+import Header from '../component/common/Header';
 
 function Layout() {
 	return (
 		<>
-			<div style = {{display: 'flex'}}>
-				<Navbar/>
-				<Outlet/>
+			<div style={{ display: 'flex' }}>
+				<Navbar />
+				<div>
+					<Header />
+					<Outlet />
+				</div>
 			</div>
 		</>
 	);
