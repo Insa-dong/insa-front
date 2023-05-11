7b04c1000a767355b6c89217e2a70ec143f10a3d
rionqkd520@gmail.com <rionqkd520@gmail.com>
2023-05-11 12:01:02 +0900

    💄 Design: emp 검색

--- src/pages/emp/Emp.js
+++ src/pages/emp/Emp.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Header from '../../component/common/Header'
+import './Emp.css';
 
 function Emp() {
 
   const title = "구성원";
   return (
     <>
-      <Header title = {title}/>
-      <div>Emp</div>
+      <Header title={title} />
+      <div className="EmpWrapper">
+        <div className="EmpSearchBox">
+          <select id="EmpSelect">
+            <option value="dept">부서</option>
+            <option value="job">직책</option>
+          </select>
+
+          <input type="text" id="search" placeholder="  검색어를 입력하세요" />
+
+          <button className="EmpSearchBtn">
+            <img src="/images/search.png" alt="검색"/>
+          </button>
+          
+        </div>
+      </div>
     </>
   )
 }
