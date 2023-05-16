f2fa3b41582c7330ed3522387d8ff530ba784973
imyueol <imyueol000@gmail.com>
2023-05-16 15:38:01 +0900

    commit

--- src/pages/board/BoardDetail.js
+++ src/pages/board/BoardDetail.js
@@ -8,22 +8,41 @@ function BoardDetail() {
     return (
         <>
             <Header title={title} />
-            <div className={CSS.profile}>
-            <div className="sideBox">
+            <div className={CSS.boardWrapper}>
+                <div className={CSS.profile}>
+                    <div className="sideBox">
 
-                <div id="prof"></div>
+                        <div id="prof"></div>
 
-                <div className="sideTxt">
-                    <span className="topName">김영한</span>
-                    <span className="topAuth">행정팀 관리자</span>
+                        <div className="sideTxt">
+                            <span className="topName">김영한</span>
+                            <span className="topAuth">행정팀 관리자</span>
+                        </div>
+                    </div>
+                </div>
+                <ul style={{ display: 'flex' }}>
+                    <li className={CSS.title}>제목</li>
+                    <li className={CSS.date}>2023-05-16</li>
+                </ul>
+                <div className={CSS.maincontent}>
+                    <div className={CSS.maintext}>내용</div>
+                </div>
+                <div className={CSS.btn}>
+                <div className="bbttnn">
+                    <button id="loginBtn"
+                        // onClick={onClickLogin}
+                    >
+                        수정하기
+                    </button>
+                    <button id="pwBtn"
+                        // onClick={() => navigate("/pwsearch")}
+                    >
+                        삭제하기
+                    </button>
+                </div>
                 </div>
             </div>
-            </div>
-            <ul style={{ display: 'flex' }}>
-                <li>제목</li>
-                <li>2023-05-16</li>
-            </ul>
-            
+
         </>
 
     );
