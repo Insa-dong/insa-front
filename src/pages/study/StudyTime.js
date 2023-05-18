698511d26aaa8803840ec28fab67ce794feb63f1
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-18 12:21:21 +0900

    asdfsafasdf

--- src/pages/study/StudyTime.js
+++ src/pages/study/StudyTime.js
@@ -1,21 +1,43 @@
 import {useState} from "react";
 import DateCSS from './StudyTime.module.css';
 
-function StudyTime({studyTimes, readOnly, form, setForm}) {
+function StudyTime({studyTimes, readOnly, form, setForm, studyDate, setStudyDate}) {
 
 	const [dates, setDates] = useState(["월", "화", "수", "목", "금"]);
 	const [day, setDay] = useState([]);
-
-	const onChangeHandler = (e) => {
-		day.push({
-			[e.target.name]: e.target.value
+	const makeInitialDay = () => {
+		const initialDay = dates.map(date => {
+			const find = studyTimes.find(time => time.studyDate === date);
+			let startTime = '';
+			let endTime = '';
+			if (find) {
+				startTime = find.startTime;
+				endTime = find.endTime;
+			}
+			return {
+				startTime, endTime
+			}
 		});
+
+		setDay(initialDay);
+	}
+	const onChangeHandler = (e, index) => {
+		console.log(e);
+		console.log(index);
+		if (day.length === 0) makeInitialDay();
+		let temp = day.map(i => i);
+		temp[index] = ({
+			...temp[index],
+			[e.target.name]: e.target.value
+		})
+		setDay(temp);
 		setForm({
 			...form,
 			day
-		});
+		})
 	}
 
+	console.log('day', day);
 	const mapArrayToDate = (dateArray) => {
 
 		return dateArray.map((date, index) => {
@@ -28,14 +50,14 @@ function StudyTime({studyTimes, readOnly, form, setForm}) {
 						        className = {DateCSS.calendarDateTime}
 						        defaultValue = {readOnly ? find.studyStartTime : form.study.studyTimes.studyStartTimes}
 						        readOnly = {readOnly}
-						        onChange = {onChangeHandler}
-						        name = {`${index}.startTime`}
+						        onChange = {(val) => onChangeHandler(val, index)}
+						        name = "startTime"
 						/>}
 						{<input type = "time" className = {DateCSS.calendarDateTime2}
-						        onChange = {onChangeHandler}
+						        onChange = {(val) => onChangeHandler(val, index)}
 						        defaultValue = {readOnly ? find.studyEndTime : form.study.studyTimes.studyEndTimes}
 						        readOnly = {readOnly}
-						        name = {`${index}.endTime`}
+						        name = "endTime"
 						/>}
 					</div>
 				);
@@ -45,13 +67,13 @@ function StudyTime({studyTimes, readOnly, form, setForm}) {
 					<input type = "time"
 					       className = {DateCSS.calendarDateTime}
 					       readOnly = {readOnly}
-					       onChange = {onChangeHandler}
-					       name = {`${index}.startTime`}
+					       onChange = {(val) => onChangeHandler(val, index)}
+					       name = "startTime"
 					/>
 					{<input type = "time" className = {DateCSS.calendarDateTime2}
-					        onChange = {onChangeHandler}
+					        onChange = {(val) => onChangeHandler(val, index)}
 					        readOnly = {readOnly}
-					        name = {`${index}.endTime`}
+					        name = "endTime"
 					/>}
 
 				</div>
