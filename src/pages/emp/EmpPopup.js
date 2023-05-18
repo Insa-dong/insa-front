274116e8bdb6a842c73c5602b46ea2bae5e6497a
rionqkd520@gmail.com <rionqkd520@gmail.com>
2023-05-18 17:22:53 +0900

    ✨ Feat:  구성원 상세 페이지 연결

---
+++ src/pages/emp/EmpPopup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './EmpPopup.css';
+
+function EmpPopup() {
+    return (
+        <>
+            <div className="EmpPopupWrap">
+                
+            </div>
+        </>
+    )
+}
+
+export default EmpPopup
