90e523e5e3d2650f37fde85a20ae47e03e64cceb
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-23 21:01:06 +0900

    calList.map 해결하기

--- src/modules/CalendarModule.js
+++ src/modules/CalendarModule.js
@@ -3,14 +3,17 @@ import {createActions, handleActions} from "redux-actions";
 const initialState = [];
 
 const GET_CAL_LIST = 'calendar/GET_CAL_LIST';
+const PUT_CAL_LIST = 'calendar/PUT_CAL_LIST'
 
-export const {calendar: {getCalList}} = createActions({
-	[GET_CAL_LIST]: res => res.data
+export const {calendar: {getCalList, putCalList}} = createActions({
+	[GET_CAL_LIST]: res => res.data,
+	[PUT_CAL_LIST]: res => res
 });
 
 const calendarReducer = handleActions(
 	{
-		[GET_CAL_LIST]: (state, {payload}) => payload
+		[GET_CAL_LIST]: (state, {payload}) => payload,
+		[PUT_CAL_LIST]: (state, {payload}) => ({modify: payload})
 	}, initialState
 )
 
