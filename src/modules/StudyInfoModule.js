ed97ac31cdc69a1d4d38275eb99f760e76e2482c
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-23 10:19:41 +0900

    study / training 끝

--- src/modules/StudyInfoModule.js
+++ src/modules/StudyInfoModule.js
@@ -6,14 +6,29 @@ const GET_STUDYINFO = 'studyInfo/GET_STUDYINFO';
 const GET_STUDYINFO_LIST = 'studyInfo/GET_STUDYINFOLIST';
 const PUT_STUDYINFO = 'studyInfo/PUT_STUDYINFO';
 const POST_STUDYINFO = 'studyInfo/POST_STUDYINFO';
+const DELETE_STUDYINFO = 'studyInfo/DELETE_STUDYINFO';
 const GET_MY_STUDY = 'studyInfo/GET_MY_STUDY';
+const INIT_STUDYINFO = 'studyInfo/INIT_STUDYINFO';
 
-export const {studyInfo: {getStudyinfolist, getStudyinfo, putStudyinfo, getMyStudy, postStudyinfo}} = createActions({
+export const {
+	studyInfo: {
+		getStudyinfolist,
+		getStudyinfo,
+		putStudyinfo,
+		getMyStudy,
+		postStudyinfo,
+		deleteStudyinfo,
+		initStudyinfo
+	}
+} = createActions({
 	[GET_STUDYINFO_LIST]: res => res.data,
 	[GET_STUDYINFO]: res => res.data,
 	[PUT_STUDYINFO]: res => res,
 	[POST_STUDYINFO]: res => res,
-	[GET_MY_STUDY]: res => res.data
+	[DELETE_STUDYINFO]: res => res,
+	[GET_MY_STUDY]: res => res.data,
+	[INIT_STUDYINFO]: () => {
+	}
 })
 
 const studyInfoReducer = handleActions(
@@ -22,7 +37,9 @@ const studyInfoReducer = handleActions(
 		[GET_STUDYINFO]: (state, {payload}) => payload,
 		[PUT_STUDYINFO]: (state, {payload}) => ({modify: payload}),
 		[POST_STUDYINFO]: (state, {payload}) => ({regist: payload}),
-		[GET_MY_STUDY]: (state, {payload}) => payload
+		[GET_MY_STUDY]: (state, {payload}) => payload,
+		[DELETE_STUDYINFO]: (state, {payload}) => ({remove: payload}),
+		[INIT_STUDYINFO]: () => initialState
 	}, initialState
 )
 
