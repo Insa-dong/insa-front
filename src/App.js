666e624136850bd0465db913707c8f6b18dc4cf3
imyueol <imyueol000@gmail.com>
2023-05-23 09:27:56 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -29,15 +29,7 @@ import StudyRegistration from "./pages/study/StudyRegistration";
 import Training from "./pages/training/Training";
 import TrainingDetail from "./pages/training/TrainingDetail";
 import TrainingRegistration from "./pages/training/TrainingRegistration";
-import BoardDetail from './pages/board/BoardDetail';
-import EmpRegistration from './pages/emp/EmpRegistration';
-import EmpDetail from './pages/emp/EmpDetail';
-import Off from './pages/off/Off';
-import AdminOff from './pages/off/AdminOff';
-import OffNow from './pages/off/OffNow';
-import Mypage from './pages/mpg/Mypage';
-import EmpTeacher from './pages/emp/EmpTeacher';
-import EmpTeacherDetail from './pages/emp/EmpTeacherDetail';
+
 
 
 
