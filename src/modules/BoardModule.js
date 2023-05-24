740d9d34df5e754a3e2464ec7aae714d964478bd
imyueol <imyueol000@gmail.com>
2023-05-24 21:16:44 +0900

    commit

--- src/modules/BoardModule.js
+++ src/modules/BoardModule.js
@@ -6,16 +6,19 @@ const initialState = [];
 /* 액션 */
 const GET_BOARDLIST = 'board/GET_BOARDLIST';
 const POST_BOARD = 'board/POST_BOARD';
+const GET_BOARD = 'board/GET_BOARD';
 
-export const { board : { getBoardlist, getBoardsearch, postBoard } } = createActions({
+export const { board : { getBoardlist, getBoardsearch, postBoard, getBoard } } = createActions({
     [GET_BOARDLIST] : res => res.data,
-    [POST_BOARD] : res => res
+    [POST_BOARD] : res => res,
+    [GET_BOARD] : res => res.data
 });
 
 /* 리듀서 */
 const boardReducer = handleActions({
     [GET_BOARDLIST] : (state, { payload }) => payload,
-    [POST_BOARD] : (state, { payload }) => ({ regist : payload })
+    [POST_BOARD] : (state, { payload }) => ({ regist : payload }),
+    [GET_BOARD] : (state, { payload }) => ({ detail : payload })
 }, initialState);
 
 export default boardReducer;
