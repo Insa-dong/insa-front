7e946e9db6735f31be693d962bad30606c4e7948
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-05-26 17:12:42 +0900

    cal

--- src/component/common/Main.js
+++ src/component/common/Main.js
@@ -1,22 +1,9 @@
-import React, {useState} from "react";
-import ModifyButton from "../calendar/ModifyButton";
-import MyCalendar from "../calendar/MyCalendar";
-import CSS from "./Main.module.css"
-
 function Main() {
-
-	const [isRegistOpen, setIsRegistOpen] = useState(false);
 
 
 	return (
 		<>
-			<div className = {CSS.mainContainer}>
-				<div className = {CSS.Calendar}>
-					<MyCalendar/>
-					<ModifyButton isRegistOpen = {isRegistOpen}
-					              setIsRegistOpen = {setIsRegistOpen}></ModifyButton>
-				</div>
-			</div>
+			메인
 		</>
 	);
 }
