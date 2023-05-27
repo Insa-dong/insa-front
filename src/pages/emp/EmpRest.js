5d18112b818170b40f586e1b6dd89cc7ba6d286d
rionqkd520@gmail.com <rionqkd520@gmail.com>
2023-05-28 01:36:39 +0900

    ✨ Feat: 휴직 내역 조회

--- src/pages/emp/EmpRest.js
+++ src/pages/emp/EmpRest.js
@@ -2,10 +2,28 @@ import { useEffect, useState } from 'react'
 import Header from '../../component/common/Header'
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { callEmpRestList } from '../../apis/EmpAPICalls';
+import RestList from '../../component/lists/RestList';
+import PagingBar from './../../component/common/PagingBar';
+
 
 function EmpRest() {
-
+ 
     const title = "구성원";
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {rest} = useSelector(state => state.empReducer);
+    const [currentPage, setCurrentPage] = useState(1);
+    
+    console.log('rest : ', {rest});
+
+    
+
+    useEffect(
+        () => {
+            dispatch(callEmpRestList({ currentPage }))
+        }, [currentPage]
+    );
 
     return (
         <>
@@ -43,6 +61,14 @@ function EmpRest() {
                     >
                         <img src="/images/search.png" alt="검색" />
                     </button>
+                </div>
+
+                <div>
+                    {rest?.data && <RestList restList={rest.data}/>}
+                </div>
+
+                <div>
+                {rest?.pageInfo && <PagingBar pageInfo={rest.pageInfo} setCurrentPage={setCurrentPage} />}
                 </div>
             </div>
         </>
