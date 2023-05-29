c96692372b7737087016ebbd2461094f46bd086b
imyueol <imyueol000@gmail.com>
2023-05-29 23:25:01 +0900

    commit

--- src/modules/BoardModule.js
+++ src/modules/BoardModule.js
@@ -7,18 +7,24 @@ const initialState = [];
 const GET_BOARDLIST = 'board/GET_BOARDLIST';
 const POST_BOARD = 'board/POST_BOARD';
 const GET_BOARD = 'board/GET_BOARD';
+const PUT_BOARD = 'board/PUT_BOARD';
+const DELETE_BOARD ='board/DELETE_BOARD';
 
-export const { board : { getBoardlist, getBoardsearch, postBoard, getBoard } } = createActions({
+export const { board : { getBoardlist, getBoardsearch, postBoard, getBoard, putBoard, deleteBoard } } = createActions({
     [GET_BOARDLIST] : res => res.data,
     [POST_BOARD] : res => res,
-    [GET_BOARD] : res => res.data
+    [GET_BOARD] : res => res.data,
+    [PUT_BOARD] : res => res,
+    [DELETE_BOARD] : res => res,
 });
 
 /* 리듀서 */
 const boardReducer = handleActions({
     [GET_BOARDLIST] : (state, { payload }) => payload,
     [POST_BOARD] : (state, { payload }) => ({ regist : payload }),
-    [GET_BOARD] : (state, { payload }) => ({ detail : payload })
+    [GET_BOARD] : (state, { payload }) => ({ detail : payload }),
+    [PUT_BOARD] : (state, { payload }) => ({ update : payload }),
+    [DELETE_BOARD] : (state, { payload }) => ({ erase : payload }),
 }, initialState);
 
 export default boardReducer;
