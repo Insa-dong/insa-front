8e5bcad606193a3bec319db71de59686e0aa20b0
junstone <126157268+dlwnstjr0310@users.noreply.github.com>
2023-06-02 16:07:10 +0900

    근태 권한체크

--- src/component/router/ProtectedRoute.js
+++ src/component/router/ProtectedRoute.js
@@ -2,11 +2,15 @@ import {Navigate} from "react-router-dom";
 import {isAdmin, isLogin} from "../../utils/TokenUtils";
 import TeacherNavbar from "../common/TeacherNavbar";
 
-function ProtectedRoute({loginCheck, authCheck, children}) {
+function ProtectedRoute({loginCheck, authCheck, adminCheck, children}) {
 
 	if (authCheck) {
 		const admin = isAdmin();
 		return admin.length > 0 ? children : <TeacherNavbar/>
+	}
+	if (adminCheck) {
+		const admin = isAdmin();
+		return admin.length > 0 ? children : ''
 	}
 	if (loginCheck) {
 		return isLogin() ? children : <Navigate to = "/login"/>
